042bc5917d45cce8911d84c8981664e338641736
Prayag Krishna <112001032@smail.iitpkd.ac.in>
2023-05-03 10:20:53 +0530

    basic functionality added for reservation and cancellation, ui updates required

--- start.py
+++ start.py
@@ -76,14 +76,17 @@ class RailwayManagementSystemGUI:
         self.main_menu_frame.pack(pady=50)
         Label(self.main_menu_frame, text="Welcome to Railway Management System", font=("Helvetica", 20)).pack(side=TOP, pady=20)
 
-        # Button to display trains between stations
-        Button(self.main_menu_frame, text="Trains Between Stations", font=("Helvetica", 16), command=self.trains_between_stations).pack(pady=10)
-
         # Button to login page
         Button(self.main_menu_frame, text="Login", font=("Helvetica", 16), command=self.login_page).pack(pady=10)
 
         #Button to add a reservation page
         Button(self.main_menu_frame, text="Reserve Seat", font=("Helvetica", 16), command=self.reserve_tickets_page).pack(pady=10)
+        
+        # Button to display trains between stations
+        Button(self.main_menu_frame, text="Trains Between Stations", font=("Helvetica", 16), command=self.trains_between_stations).pack(pady=10)
+        
+        #Button to add a cancellation page
+        Button(self.main_menu_frame, text="Cancel Seat", font=("Helvetica", 16), command=self.cancel_seat_page).pack(pady=10)
         
     def trains_between_stations(self):
         # Clear the main menu frame
@@ -220,6 +223,7 @@ class RailwayManagementSystemGUI:
         # Create a button to submit the login information
         Button(self.login_frame, text="Login", font=("Helvetica", 16), command=self.login).pack(pady=10)
 
+        
     def login(self):
         # Get the values from the entry boxes
         username = self.username_entry.get()
@@ -280,15 +284,15 @@ class RailwayManagementSystemGUI:
         self.pass_entry = Entry(self.input_frame)
         self.pass_entry.grid(row=7, column=1)
         
-        self.result_label= Label(self.input_frame, text="Reservation Status:")
-        self.result_label.grid(row=8, column=0)
-        self.result_output = Entry(self.input_frame)
-        self.result_output.grid(row=8, column=1)
-        
         # Create a button to execute the query
         self.reserve_button = Button(self.input_frame, text="Reserve Seats", command=self.reserve_seat)
-        self.reserve_button.grid(row=9, column=0, columnspan=2, pady=10)
-
+        self.reserve_button.grid(row=8, column=0, columnspan=2, pady=10)
+        
+        self.result_label= Label(self.input_frame, text="PNR No:")
+        self.result_label.grid(row=9, column=0)
+        self.result_output = Entry(self.input_frame)
+        self.result_output.grid(row=9, column=1)
+        
         
     def reserve_seat(self):
         tno = self.tno_entry.get()
@@ -316,15 +320,69 @@ class RailwayManagementSystemGUI:
         except ValueError:
             messagebox.showerror("Error", "Invalid date format. Please use DD-MM-YYYY")
             return
-        query = f"CALL reserve_seat('{tno}', '{src}', '{dst}', '{doj}', '{c_typ}', '{usr_id}', '{trxn_id}', '{pass_ids}');"
-        result = db.execute_dql_commands(query)
-        if result:
-            # Show a message box to indicate successful reservation
+        query1 = f"CALL reserve_seat('{tno}', '{src}', '{dst}', '{doj}', '{c_typ}', {usr_id}, {trxn_id}, '{pass_ids}');"
+        values = {"tno": tno, "src": src, "dst": dst, "doj": doj, "c_typ": c_typ, "usr_id": usr_id, "trxn_id": trxn_id, "pass_ids": pass_ids}
+        try:
+            db.execute_ddl_and_dml_commands(query1, values)
             messagebox.showinfo("Success", "Reservation successful.")
-            self.result_output.config(text="Reservation successful. PNR number: " + str(result))
-        else:
-            self.result_output.config(text="Reservation failed.")
+            self.result_output.config(text=" 12")
+        except:
+            self.result_output.config(text="Reservation failed. Error")
+    
+    
+    def cancel_seat_page(self):
 
+        # Clear the main menu frame
+        self.main_menu_frame.destroy()
+
+        # Create a frame to hold the input widgets
+        self.input_frame = Frame(self.master)
+        self.input_frame.pack(side=LEFT, padx=10, pady=10)
+        
+        self.pnr_no_label= Label(self.input_frame, text="PNR No:")
+        self.pnr_no_label.grid(row=0, column=0)
+        self.pnr_no_entry = Entry(self.input_frame)
+        self.pnr_no_entry.grid(row=0, column=1)
+        
+        self.uid_label= Label(self.input_frame, text="User ID:")
+        self.uid_label.grid(row=1, column=0)
+        self.uid_entry = Entry(self.input_frame)
+        self.uid_entry.grid(row=1, column=1)
+        
+        self.pass_label= Label(self.input_frame, text="Passenger IDs (comma-separated):")
+        self.pass_label.grid(row=2, column=0)
+        self.pass_entry = Entry(self.input_frame)
+        self.pass_entry.grid(row=2, column=1)
+        
+        # Create a button to execute the query
+        self.cancel_button = Button(self.input_frame, text="Cancel seats", command=self.cancel_seat)
+        self.cancel_button.grid(row=3, column=0, columnspan=2, pady=10)
+        
+        self.result_label= Label(self.input_frame, text="Cancellation status:")
+        self.result_label.grid(row=4, column=0)
+        self.result_output = Entry(self.input_frame)
+        self.result_output.grid(row=4, column=1)
+        
+        
+    def cancel_seat(self):
+        pnr_no = self.pnr_no_entry.get()
+        usr_id = self.uid_entry.get()
+        pass_ids = self.pass_entry.get()
+        
+        if not pnr_no or not usr_id or not pass_ids:
+            messagebox.showerror("Error", "Please enter all required fields")
+            return
+        query1 = f"CALL cancel_seat('{pnr_no}', {usr_id}, '{pass_ids}');"
+        values = {"pnr_no": pnr_no,"usr_id": usr_id, "pass_ids": pass_ids}
+        result = db.execute_dql_commands(query1)
+        try:
+            db.execute_ddl_and_dml_commands(query1, values)
+            # Show a message box to indicate successful reservation
+            messagebox.showinfo("Success", "Cancellation successful.")
+            self.result_output.config(text="Cancellation successful. Refunded: " )
+        except:
+            self.result_output.config(text="Cancellation failed. Error")
+        
 
 def main():
     root=Tk()
